3e86fda9c271ba983f4aea41c5e797ebcfcc34a6
hmagharbi-bit <hmagharbi@gmail.com>
2026-03-03 05:04:23 +0100

    Fix: correct schema import paths using relative dots

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,7 +1,7 @@
+// src/sanity/schemaTypes/index.ts
 import { type SchemaTypeDefinition } from "sanity";
-import { product } from "./schemas/product";
-import { order } from "./schemas/order";
-
+import { product } from "../schemas/product";
+import { order } from "../schemas/order";
 export const schema: { types: SchemaTypeDefinition[] } = {
-    types: [product, order],
+  types: [product, order],
 };
