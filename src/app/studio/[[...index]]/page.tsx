34fbb4dfb063904b6c078cfd9aaf710ea55eef95
hmagharbi-bit <hmagharbi@gmail.com>
2026-03-03 16:37:23 +0100

    Fix: Cleaned directory structure and upgraded React to RC

--- src/app/studio/[[...index]]/page.tsx
+++ src/app/studio/[[...index]]/page.tsx
@@ -1,13 +1,7 @@
 'use client'
 
-/**
- * This route is responsible for rendering the Sanity Studio.
- * It uses the catch-all route to allow for internal studio navigation.
- */
-
 import { NextStudio } from 'next-sanity/studio'
 import config from '@/sanity/config'
-
 
 export default function StudioPage() {
     return <NextStudio config={config} />
--- src/sanity/[[...index]]/page.tsx.ts
+++
@@ -1,7 +0,0 @@
-import { type SchemaTypeDefinition } from "sanity";
-import { product } from "@/sanity/schemas/product";
-import { order } from "@/sanity/schemas/order";
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-    types: [product, order],
-};
